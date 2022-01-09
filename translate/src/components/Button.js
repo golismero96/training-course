e597fb95c9feb8f5f603985d4103946ad8d875cc
golismero <golismero96@gmail.com>
2022-01-09 19:38:51 +0330

    start context

--- translate/src/components/Button.js
+++ translate/src/components/Button.js
@@ -1,7 +1,10 @@
-import React from 'react';
+import React, {useContext} from 'react';
+import LanguageContext from '../contexts/LanguageContext';
 
 
 const Button = () => {
+    const things = useContext(LanguageContext)
+    console.log(things)
     return (
         <button className="button ui primary">
             Submit
@@ -9,5 +12,6 @@ const Button = () => {
     );
 };
 
+// Button.contextType = LanguageContext
 
 export default Button;
