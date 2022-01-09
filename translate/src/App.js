83ef243cd96d6c50f5b87bca35aa67d002fb0b0c
golismero <golismero96@gmail.com>
2022-01-09 19:08:04 +0330

    set flag in page

--- translate/src/App.js
+++ translate/src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [language, setLanguage] = useState('english')
+  const onChangeLanguage = (lan) => {
+    setLanguage(lan)
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+          Select a language:
+          <i className="flag us" onClick={() => onChangeLanguage('english')} />
+          <i className="flag ir" onClick={() => onChangeLanguage('persian')} />
+      </div>
+      <p>{language}</p>
     </div>
   );
 }
