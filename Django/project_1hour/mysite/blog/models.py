348cf62b96990fe19029e2da70eaf969b9696d69
golismero <golismero96@gmail.com>
2022-01-31 21:11:28 +0330

    Ordering codes

--- Django/project_1hour/mysite/blog/models.py
+++ Django/project_1hour/mysite/blog/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 from django.utils import timezone
 from django.contrib.auth.models import User
 
@@ -15,6 +16,7 @@ class Post (models.Model):
     )
 
     title = models.CharField(max_length=250)
+    excerpt = models.TextField(null=True)
     slug = models.SlugField(max_length=250, unique_for_date='publish')
     publish = models.DateTimeField(default=timezone.now)
     author = models.ForeignKey(
@@ -23,6 +25,9 @@ class Post (models.Model):
     status = models.CharField(max_length=10, choices=options, default='draft')
     objects = models.Manager()  # default manager
     newmanager = NewManager()  # custom manager
+
+    def get_absolute_url(self):
+        return reverse('blog:post_single', args=[self.slug])
 
     class Meta:
         ordering = ('publish', )
