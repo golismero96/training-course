eb0c4ea1ff7c35070430953875f93def287843dd
golismero <golismero96@gmail.com>
2022-02-26 23:16:32 +0330

    delete user with command

--- Django/django_api_react/blog/management/commands/usergen.py
+++ Django/django_api_react/blog/management/commands/usergen.py
@@ -6,12 +6,19 @@ class Command(BaseCommand):
     help = 'DB random user generator'
     def add_arguments(self, parser):
         parser.add_argument('qty', type=int, help='The amount of users to create')
+        parser.add_argument('-a', '--admin', action='store_true', help='define an admin account')
 
     def handle(self, *args, **kwargs):
         qty = kwargs['qty']
+        admin = kwargs['admin']
 
         for i in range(qty):
             username = get_random_string(10)
             password = get_random_string(10)
 
-            User.objects.create_user(username=username, email='', password=password)
+            if admin:
+                User.objects.create_superuser(username=username, email='', password=password)
+            else:
+                User.objects.create_user(username=username, email='', password=password)
+            self.stdout.write('User "%s (%s)" account created!' % (username, password))
+
