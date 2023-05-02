8b23cca4aa2d8c925c016e8679cf4866861655d9
golismero <golismero96@gmail.com>
2023-05-02 23:31:42 +0430

    add stamp image

--- Next.js/First-Priority/sections/Hero.jsx
+++ Next.js/First-Priority/sections/Hero.jsx
@@ -29,8 +29,13 @@ const Hero = () => (
         variants={slideIn('right', 'tween', 0.2, 1)}
         className='relative w-full md:-mt-[20px] -mt-[12px]'
       >
-        <div className="absolute w-full h-[300px] hero-gradient rounded-tl-[140px] z-[0] -top-[30px]">
+        <div className="w-full h-[500px] hero-gradient rounded-tl-[140px] z-[0] -top-[30px]">
           <img src="/cover.png" alt="cover" className="w-full sm:h-[500px] h-[350px] object-cover rounded-tl-[140px] z-10 relative" />
+          <a href="#explore">
+            <div className="w-full flex justify-end sm:-mt[70px] -mt-[50px] pr-[40px] relative z-10">
+              <img src="/stamp.png" alt="stamp" className='sm:w-[155px] w-[100px] sm:h-[155px] h-[100px] object-content' />
+            </div>
+          </a>
         </div>
       </motion.div>
 
