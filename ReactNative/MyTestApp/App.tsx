feb21bc2e7eb4dba9e5e299effaff58068dbd907
golismero <golismero96@gmail.com>
2023-05-05 23:32:33 +0430

    start first my priject react-native

---
+++ ReactNative/MyTestApp/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+export default function App() {
+  return (
+    <View style={{ padding: 50 }}>
+      <Text>Hello World</Text>
+      <Text>Green</Text>
+      <Text>White</Text>
+      <Text>Red</Text>
+    </View>
+  )
+}
