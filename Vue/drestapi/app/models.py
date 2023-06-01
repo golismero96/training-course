8bb698a357d9e41670cddbabe2a4a62f7c9ab318
golismero <golismero96@gmail.com>
2023-06-01 19:43:55 +0330

    remove vue

--- Vue/drestapi/app/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-class Posts(models.Model):
-    title = models.CharField(max_length=250)
-    content = models.TextField()
-
-    def __str__(self):
-        return self.title
-    
--- Vue/drestapi/app/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import PostsView
-
-urlpatterns = [
-    path('posts/', PostsView.as_view(), name='posts_view')
-]
